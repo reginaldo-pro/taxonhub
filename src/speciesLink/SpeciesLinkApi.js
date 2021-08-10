44f959e368cedc77bd7c64382a57dd9cc3bc7970
JMallone <michels@alunos.utfpr.edu.br>
2021-08-09 23:25:46 -0300

    Ajustando a Api

--- src/speciesLink/SpeciesLinkApi.js
+++ src/speciesLink/SpeciesLinkApi.js
@@ -1,65 +1,112 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-import axios from 'axios'
+import axios from 'axios';
 
-function api() {
-  fetch('http://api.splink.org.br/datasets/format/json', {
-    method: 'GET',
-    headers: {
-    'Content-Type': 'application/json',
-    //'Access-Control-Allow-Origin': '*'
-    'Origin': 'https://api.splink.org.br'
-    }
-  })
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
-
-/*
- *      headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-          }
- */
-function api2() {
-  console.log("-------------");
-  const fetchData = () => {
-    axios.get('https://api.splink.org.br/records/ScientificName/Eichhornia%20azurea/Synonyms/flora2020/Format/JSON',{
-    headers:{
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-        "Origin": "*"
-    }
-
+function api2(lista) {
+    const param = lista.join("/")
+    console.log("OIA A LISTAAAA ",lista)
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.splink.org.br/records/ScientificName/${param}/Synonyms/flora2020/Format/JSON`)
+    .then(response => {
+        return response.json()
     })
-        .then((response) => console.log(response.data))
-        .catch((err) => {
-            console.error("Erro: " + err);
-  });
 }
-  fetchData();
-  console.log("-------------");
-}
-function test(){
-  _download = async (sp_names) => {
-        return await new Promise((resolve, reject) => {
-            return axios.get(
-                'https://api.splink.org.br/records/ScientificName/Eichhornia%20azurea/Synonyms/flora2020/Format/JSON'
-            ).then(response => {
-                console.log("[SPL] Download de: " + response.data.result.length + " ocorrências.")
-                resolve(response.data.result)
-            }).catch(er => {
-                reject(er)
-            })
-        })
-  }
-}
+
+
+const mockData = (([
+            "Eichhornia azurea"
+            ,"Cantinoa althaeifolia"
+            ,"Abildgaardia ovata"
+            ,"Abolboda pulchella"
+            ,"Acisanthera inundata"
+            ,"Rauvolfia sellowii"
+            ,"Cantinoa althaeifolia"
+            ,"Abarema turbinata",
+                    "Comolia villosa",
+                    "Stylosanthes campestris",
+                    "Vochysia floribunda",
+                    "Eugenia platyphylla",
+                    "Chaetocalyx acutifolia",
+                    "Actinocephalus ochrocephalus",
+                    "Cicuta douglasii",
+                    "Annona senegalensis",
+                    "Hexasepalum radula",
+                    "Trichosalpinx dura",
+                    "Allophylus sericeus",
+                    "Microlicia juniperina",
+                    "Campomanesia pubescens",
+                    "Luxemburgia ciliosa",
+                    "Dalechampia humilis",
+                    "Croton heliotropiifolius",
+                    "Senna alata",
+                    "Ipomoea dichotoma",
+                    "Arundo donax"]));
+
+
 class SpeciesLinkApi extends Component {
-    render() {
-        api2();
-        const mockData = [ "Eichhornia azurea", "Cantinoa althaeifolia" ];
-        const lista = mockData.map((number) =>
-            <li>{number}</li>
+
+      constructor(props) {
+        super(props);
+        this.state = {error : null, isLoaded : false, items : [], count: 0, chunks: 0};
+      }
+
+      componentDidCatch(error, errorInfo) {
+          console.log("ERRO: " + error);
+          console.log("ERRO-INFO: " + errorInfo);
+       }
+
+        componentDidMount() {
+          const getPages = (i) => {
+            this.setState({
+                count : this.state.count+1
+            })
+            const start = i * 5;
+            const end = start + 5;
+            const list = mockData.slice(start, end);
+            if(list.length == 0){
+              this.setState({
+                isLoaded : true
+                })
+              return ;
+            }
+
+            api2(list)
+                .then(data => {
+                    const result = this.state.items.concat(data.result)
+
+                    this.setState({
+                      items : result
+                     });
+
+                    getPages(i+1)
+                })
+            .catch(error => { this.setState({error : error.message}); })
+
+          };
+          const length = mockData.length;
+          const chunks = Math.ceil(length / 5);
+          this.setState({
+            chunks : chunks
+          })
+          getPages(0);
+        }
+
+        render() {
+
+        if (this.state.error) {
+            return <h1>{this.state.error}</h1>
+        }
+
+        if(!this.state.isLoaded){
+            return <h1>Carregando {this.state.count}/{this.state.chunks}</h1>
+        }
+
+        console.log(this.state.items)
+
+        const items = this.state.items.map(x => {
+            return <li key={x.record_id+x.scientificName}> {x.record_id}:{x.institutionCode}     {x.county}</li>
+        });
+             const lista = mockData.map((number) =>
+            <li key={number.toString()}>{number}</li>
         );
 
         return (
@@ -67,9 +114,10 @@ class SpeciesLinkApi extends Component {
                 <h1>API do Species Link :^)</h1>
                 <b>Lista de Nomes:</b>
                 {lista}
+                {items}
             </div>
         );
     }
-}
+      }
 
 export default hot(module)(SpeciesLinkApi);
