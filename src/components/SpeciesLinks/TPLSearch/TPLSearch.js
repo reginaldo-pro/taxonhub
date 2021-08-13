080dad628140177add01222a7077cad1d1f4ef69
luisotaviocap98 <luiscapelari@alunos.utfpr.edu.br>
2021-08-13 02:23:29 -0300

    aprimorando estrutura do arquivo

--- src/components/SpeciesLinks/TPLSearch/TPLSearch.js
+++ src/components/SpeciesLinks/TPLSearch/TPLSearch.js
@@ -1,25 +1,28 @@
-// var SpeciesLinkAPI = require('../Cache/SpeciesLinkAPI')
+// var SpeciesLinkAPI = require('../API/SpeciesLinkAPI')
 // var SpeciesLinkCache = require('../Cache/SpeciesLinkCache')
-var express = require(express)
-
 
 let arraySpecies = [];
-
-const search = async function searchSpeciesList(speciesList) {
+export async function searchSpeciesList(speciesList) {
+  try {
     arraySpecies = [];
     speciesList.forEach((especie) => {
-        if (await SpeciesLinkCache.find(especie)){ //se existe em cache //talvez usar then catch
-            arraySpecies.push(especie);
-        } else{
-            if (await SpeciesLinkAPI.searcSpecies(especie)){ //se a API retornar
-                arraySpecies.push(especie);
-                SpeciesLinkCache.save(especie); //armazenar em cache
-            }
+      const hasSpecie = SpeciesLinkCache.find(especie);
+      if (hasSpecie) { //se existe em cache //talvez usar then catch
+        arraySpecies.push(especie);
+      } else {
+        let APISpecie = SpeciesLinkAPI.searcSpecies(especie);
+        if (APISpecie) { //se a API retornar
+          arraySpecies.push(especie);
+          SpeciesLinkCache.save(especie); //armazenar em cache
         }
+      }
     });
+  } catch (e) {
+    throw new Error('error');
+  } finally {
+    return arraySpecies;
+  }
 }
-
-const quantity = async function getNumberOfValidSpecies() {
-    return arraySpecies.length;
+export async function getNumberOfValidSpecies() {
+  return arraySpecies.length;
 }
-module.exports = { search, quantity }
