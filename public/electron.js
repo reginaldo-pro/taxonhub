50c008c152ee692c9b1f808dd57bbeddcfcd2c13
sorattorafa <soratto@alunos.utfpr.edu.br>
2021-08-15 22:40:47 -0300

    add menu

--- public/electron.js
+++ public/electron.js
@@ -1,52 +1,170 @@
-const electron = require('electron')
-const app = electron.app
-const path = require('path')
-const isDev = require('electron-is-dev')
-const BrowserWindow = electron.BrowserWindow
-require('electron-reload')
+const electron = require('electron');
 
-let mainWindow
+const { app } = electron;
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+const { BrowserWindow } = electron;
+require('electron-reload');
+
+let mainWindow;
+const { Menu } = require('electron');
+
+const isMac = process.platform === 'darwin';
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'Taxonhub',
+    submenu: [
+      {
+        label: 'Validar csv',
+        click: async () => {
+        //   const { shell } = electron;
+        //   await shell.openExternal('https://electronjs.org');
+        },
+      },
+      {
+        label: 'Listar ocorrências',
+        click: async () => {
+        //   const { shell } = electron;
+        //   await shell.openExternal('https://electronjs.org');
+        },
+      },
+
+      isMac ? { role: 'close' } : { role: 'quit' },
+    ],
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' },
+          ],
+        },
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' },
+      ]),
+    ],
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' },
+      ] : [
+        { role: 'close' },
+      ]),
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Saiba mais',
+        click: async () => {
+        //   const { shell } = electron;
+        //   await shell.openExternal('https://electronjs.org');
+        },
+      },
+    ],
+  },
+];
 
 function createWindow() {
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
-  })
+  });
 
   // Remove o menu de utilidades do electron/chromium
-  mainWindow.setMenuBarVisibility(false)
+  mainWindow.setMenuBarVisibility(true);
 
-  if (isDev){
-    mainWindow.webContents.openDevTools()
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
   }
-  
+
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`,
-  )
+  );
 
-
-  //mainWindow.loadURL(`file://${path.join(__dirname, '../dist/index.html')}`)
+  // mainWindow.loadURL(`file://${path.join(__dirname, '../dist/index.html')}`)
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
