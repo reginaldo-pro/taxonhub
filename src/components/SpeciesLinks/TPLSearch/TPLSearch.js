6abcbe27ffaf0b1c2a550bd9cfaa02a25c7d4475
luisotaviocap98 <luiscapelari@alunos.utfpr.edu.br>
2021-08-17 19:28:07 -0300

    corrigindo função

--- src/components/SpeciesLinks/TPLSearch/TPLSearch.js
+++ src/components/SpeciesLinks/TPLSearch/TPLSearch.js
@@ -1,22 +1,50 @@
+// http://bluebirdjs.com/docs/api/promise.each.html
+// https://css-tricks.com/why-using-reduce-to-sequentially-resolve-promises-works/
+
 // var SpeciesLinkAPI = require('../API/SpeciesLinkAPI')
 // var SpeciesLinkCache = require('../Cache/SpeciesLinkCache')
 
 let arraySpecies = [];
+// export async function searchSpeciesList(speciesList) {
+//   try {
+//     arraySpecies = [];
+//     speciesList.forEach((especie) => {
+//       const hasSpecie = SpeciesLinkCache.find(especie);
+//       if (hasSpecie) { //se existe em cache //talvez usar then catch
+//         arraySpecies.push(especie);
+//       } else {
+//         let APISpecie = SpeciesLinkAPI.searcSpecies(especie); // retorna promisse
+//         if (APISpecie) { //se a API retornar
+//           arraySpecies.push(especie);
+//           SpeciesLinkCache.save(especie); //armazenar em cache
+//         }
+//       }
+//     });
+//   } catch (e) {
+//     throw new Error('error');
+//   } finally {
+//     // eslint-disable-next-line no-unsafe-finally
+//     return arraySpecies;
+//   }
+// }
+
 export async function searchSpeciesList(speciesList) {
   try {
     arraySpecies = [];
-    speciesList.forEach((especie) => {
-      const hasSpecie = SpeciesLinkCache.find(especie);
-      if (hasSpecie) { //se existe em cache //talvez usar then catch
-        arraySpecies.push(especie);
-      } else {
-        let APISpecie = SpeciesLinkAPI.searcSpecies(especie);
-        if (APISpecie) { //se a API retornar
+    speciesList.reduce((accumulator, especie) => {
+      return accumulator.then(() => {
+        const hasSpecie = SpeciesLinkCache.find(especie);
+        if (hasSpecie) { //se existe em cache //talvez usar then catch
           arraySpecies.push(especie);
-          SpeciesLinkCache.save(especie); //armazenar em cache
+        } else {
+          let APISpecie = SpeciesLinkAPI.searcSpecies(especie); // retorna promisse
+          if (APISpecie) { //se a API retornar
+            arraySpecies.push(especie);
+            SpeciesLinkCache.save(especie); //armazenar em cache
+          }
         }
-      }
-    });
+      });
+    }, Promise.resolve());
   } catch (e) {
     throw new Error('error');
   } finally {
@@ -24,6 +52,7 @@ export async function searchSpeciesList(speciesList) {
     return arraySpecies;
   }
 }
+
 export async function getNumberOfValidSpecies() {
   return arraySpecies.length;
 }
