0f9809af35742c88cc99d3f44959590407daaa3f
julianocp <julianopetini@alunos.utfpr.edu.br>
2021-08-17 18:56:14 -0300

    removendo packge-lock.json

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>I'm configuring setting up Webpack!!!Juliano-Petini</h1>
+                <h1>I'm configuring setting up Webpack!!!</h1>
                 <p>{`The count now is: ${this.state.counter}`}</p>
                 <button onClick={this.handleClick}>Click me</button>
             </div>
