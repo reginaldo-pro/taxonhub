55cdb0f2d347c22d3e0942b37c34fc4644e2212c
tuchinski <tuchinski@alunos.utfpr.edu.br>
2021-08-22 22:35:41 -0300

    commit inicial tplValidator

---
+++ src/tplValidator.js
@@ -0,0 +1,8 @@
+import { validadeFile } from './fileUploader';
+
+function validateSpecies(filename) {
+  // Iniciando validate species
+  let retorno = validadeFile(filename);
+}
+
+module.exports = { validateSpecies };
