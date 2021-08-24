2cf83c97eaa674d5f9b8af5ea894b00da948e82e
luisotaviocap98 <luiscapelari@alunos.utfpr.edu.br>
2021-08-24 17:12:14 -0300

    corrigindo testes

--- src/tests/__tests__/SpeciesLinkSearch.test.js
+++ src/tests/__tests__/SpeciesLinkSearch.test.js
@@ -1,16 +1,28 @@
 const tplSearch = require('../../speciesLink/SpeciesLinkSearch');
 
-test('Quantidade de especies validas', () => {
-  const qnt = tplSearch.getNumberOfValidSpecies;
-  expect(qnt()).toBeDefined(); // a variavel existe
-  expect(qnt()).toHaveReturned(); // a funcao retornou
-  // expect(['especie']).toThrowError(); // caso que é pra dar erro
+describe('Testando quantidade de especies', () => {
+  test('Quantidade de especies validas', () => {
+    const qnt = tplSearch.getNumberOfValidSpecies;
+    expect(qnt()).toBeDefined(); // a variavel existe
+  });
+  test('Quantidade de especies validas', () => {
+    const qnt = tplSearch.getNumberOfValidSpecies;
+    expect(qnt()).toBeDefined(); // a variavel existe
+    // expect(['especie']).toThrowError(); // caso que é pra dar erro
+  });
 });
 
-test('Buscar por especies', () => {
-  const find = tplSearch.searchSpeciesList;
-  expect(find(['Plantae Orchidaceae'])).toBeDefined(); // a variavel existe
-  expect(find(['Plantae Orchidaceae'])).toHaveReturned(); // a funcao retornou
-  expect(find(['especie aleatoria'])).toThrowError('error'); // caso de erro
-  expect(find(['Plantae Orchidaceae', 'especie aleatoria'])).toContain('Plantae Orchidaceae'); // é para ter essas especies
+describe('Testando busca por especies', () => {
+  test('Buscar por especies', () => {
+    expect(tplSearch.searchSpeciesList(['Plantae Orchidaceae'])).toBeDefined(); // a variavel existe
+  });
+  test('Buscar por especies', () => {
+    expect(tplSearch.searchSpeciesList(['Plantae Orchidaceae'])).toHaveReturned(); // a funcao retornou
+  });
+  test('Buscar por especies', () => {
+    expect(tplSearch.searchSpeciesList(['especie aleatoria'])).toThrowError('error'); // caso de erro
+  });
+  test('Buscar por especies', () => {
+    expect(tplSearch.searchSpeciesList(['Plantae Orchidaceae', 'especie aleatoria'])).toContain('Plantae Orchidaceae'); // é para ter essas especiese
+  });
 });
