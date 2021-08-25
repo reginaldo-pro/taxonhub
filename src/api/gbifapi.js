91fb67800a83375456796989410a9ed13092651f
Gabriel Hamilton <gabrielmendes999@gmail.com>
2021-08-25 00:19:20 -0300

    Aprimorando os testes do GenericAPI

--- src/api/gbifapi.js
+++ src/api/gbifapi.js
@@ -1,20 +1,27 @@
+import 'regenerator-runtime/runtime'
 import axios from 'axios';
+axios.defaults.adapter = require('axios/lib/adapters/http')
+
+/**
+ * GBIFApi
+ * @param { String } speciesName: 
+ */
+
 
 const getSearchSpecies = async (speciesName) => { 
   return axios
-    .get('gttps://api.gbif.org/vl/occurrence/search', {taxon_key: 8225540})
+    .get('https://api.gbif.org/v1/occurrence/search' + {speciesName})
     .then((response) => {
       return response.data;
     })
     .catch((err) => {
-      throw new Error('Erro!');
+      throw new Error(err);
     });
 };
 
-const searchSpeciesApi = async (speciesName) => {
+const searchSpecies = async (speciesName) => {
   try {
     const searchedData = await getSearchSpecies(speciesName);
-    
     return searchedData;
 
   } catch (error) {
@@ -22,4 +29,4 @@ const searchSpeciesApi = async (speciesName) => {
   }
 };
 
-export default searchSpeciesApi;
+export default {searchSpecies};
