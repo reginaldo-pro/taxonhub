f2872f8980f533ba43139416bab40a6f9dc2948b
tuchinski <tuchinski@alunos.utfpr.edu.br>
2021-08-26 20:04:24 -0300

    validacoes chamada fileUploader

--- src/tplValidator.js
+++ src/tplValidator.js
@@ -2,7 +2,18 @@ import { validadeFile } from './fileUploader';
 
 function validateSpecies(filename) {
   // Iniciando validate species
-  let retorno = validadeFile(filename);
+  
+  validadeFile(filename)
+    .then((output) => {
+      console.log("validado")
+      console.log(output)
+      return(output)
+    })
+    .catch((output) => {
+      console.log("nao validado")
+      console.log(output)
+      return output
+    })
 }
 
 module.exports = { validateSpecies };
