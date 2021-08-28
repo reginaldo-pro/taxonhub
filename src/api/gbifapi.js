9d36c9f4fdbe9446c6d5fc0cccd5c0104db44b88
Gabriel Hamilton <gabrielmendes999@gmail.com>
2021-08-28 15:00:47 -0300

    Api e testes corretos

--- src/api/gbifapi.js
+++ src/api/gbifapi.js
@@ -5,26 +5,30 @@ axios.defaults.adapter = require('axios/lib/adapters/http')
 
 const getSearchSpecies = async (taxonKey) => { 
   //console.log('key',taxonKey)
-  return axios.get('https://api.gbif.org/v1/occurrence/search',{taxon_key: taxonKey})
+  return axios.get('http://api.gbif.org/v1/occurrence/search/',{params : {
+     taxon_key: taxonKey,
+     //limit: 2,
+    }})
     .then((response) => {
-      console.log(response.data)
+      //console.log(response.data)
       return response.data;
     })
     .catch((err) => {
+      //console.log(err.response.data);
       throw new Error(err);
     });
 };
 
-
-
-const searchSpecies = async (speciesName) => {
-    axios.get(`http://api.gbif.org/v1/species/match?name=${speciesName}&strict=true`)
-    .then((response) => {
-      //console.log(response.data)
-      getSearchSpecies(response.data.speciesKey);
+const searchSpeciesapi = async (speciesName) => {
+  //console.log(speciesName);  
+  return axios.get(`http://api.gbif.org/v1/species/match?name=${speciesName}&strict=true`)
+  .then((response) => {
+      //console.log('aaaaaaaaaaaaaaaaaaaaa',response.data)
+      return getSearchSpecies(response.data.speciesKey);
     }).catch((error) => {
+      //console.log('aaaaaa');
       console.log(error);
     });
 };
 
-export default searchSpecies;
+export default searchSpeciesapi;
--- src/tests/__tests__/gbifapi.test.js
+++ src/tests/__tests__/gbifapi.test.js
@@ -1,30 +1,30 @@
 import 'regenerator-runtime/runtime';
 
 //import gbifapi from '../../api/gbifapi';
-import searchSpecies from '../../api/gbifapi'
+import searchSpeciesapi from '../../api/gbifapi'
 
-jest.setTimeout(30000);
+//jest.setTimeout(30000);
 
  describe('SearchSpeciesAPI', ()=>{
-
- /*  test('Tamanho do retorno maior que 1', async () => {
+ 
+  test('Tamanho do retorno maior que 1', async () => {
     let count = 0;
-    const result = await searchSpecies('Taberna disparifolia');
-    count = Object.keys(result["result"]).length
+    const result = await searchSpeciesapi('Monstera deliciosa');
+    count = result["count"]
+    console.log(count);
     expect(count).toBeGreaterThan(1);
 });
- */
-  
-/* test('Tamanho do retorno igual 0', async () => {
-    let count = 0;
-    const result = await searchSpecies('aaaaaa');
-    count = Object.keys(result["result"]).length
-    expect(count).toBe(0);
-  }); */
 
-    test('Testing a search species', async () => {
-    const result = await searchSpecies('Alfaroa Standl');
-    console.log('Resultado: ', result)
-  });  
-  
-});  
+   test('Tamanho do retorno igual 0', async () => {
+    let count = 0;
+    const result = await searchSpeciesapi('uahsgd');
+    count = result["count"]
+    expect(count).toBeGreaterThan(1000000000);
+  }); 
+
+     /*  test('Testing a search species', async () => {
+        const result = await searchSpeciesapi('Monstera deliciosa');
+        console.log('Resultado: ', result)
+      });  
+ */
+  });
