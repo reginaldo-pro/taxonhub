88d004fed83f0af0672861bc265c9799329fcb0e
tuchinski <tuchinski@alunos.utfpr.edu.br>
2021-08-29 16:54:51 -0300

    - ajustando retorno do mock tplScraper - criando mock save - fazendo chamada do save no validateSpecies

--- src/tplValidator.js
+++ src/tplValidator.js
@@ -1,29 +1,61 @@
-import { validadeFile } from './fileUploader';
+import { validadeFile } from "./fileUploader";
 
-function tplScraper(speciesList){
+function save(speciesList) {
+  // mock save Database
+
+  return new Promise((resolve, reject) => {
+    resolve(true);
+  });
+}
+
+function tplScraper(speciesList) {
   // mock do tplScraper
 
   let mockReturn = [
     {
-      searchName: 'Eichhornia azurea',
-      returnName: 'Eichhornia azurea (Sw.) Kunth',
-      status: 'Aceito',
-      synonym: '',
-      database: 'TPL',
-      family: 'Pontederiaceae'
+      searchName: "Ananas comosus",
+      returnName: "Ananas comosus (L.) Merr.",
+      status: "Aceito",
+      synonym: "",
+      database: "TPL",
+      family: "Bromeliaceae",
     },
     {
-      searchName: "Eichhornia azurea",
-      returnName: "Eichhornia azurea rhizantha (Seub.)",
+      searchName: "Ananas comosus",
+      returnName: "Ananas comosus (L.B.Sm.) Camargo",
       status: "sinonimo",
-      synonym: "Eichhornia azurea (Sw. Kunth)",
-      database: "FDB",
-      family: "Pontederiaceae"
+      synonym: "Ananas comosus (L.) Merr.",
+      database: "TPL",
+      family: "Bromeliaceae",
     },
-  ]
+    {
+      searchName: "Ananas comosus",
+      returnName: "Ananas comosus (Baker) Coppens & F.Leal",
+      status: "sinonimo",
+      synonym: "Ananas comosus (L.) Merr.",
+      database: "TPL",
+      family: "Bromeliaceae",
+    },
+    {
+      searchName: "Euterpe oleracea",
+      returnName: "Euterpe oleracea Mart.",
+      status: "Aceito",
+      synonym: "",
+      database: "TPL",
+      family: "Arecaceae",
+    },
+    {
+      searchName: "Euterpe oleracea",
+      returnName: "Euterpe oleracea Engel",
+      status: "sinonimo",
+      synonym: "Euterpe oleracea Mart.",
+      database: "TPL",
+      family: "Arecaceae",
+    },
+  ];
   return new Promise((resolve, reject) => {
     resolve(mockReturn);
-  })
+  });
 }
 
 function validateSpecies(filename) {
@@ -32,16 +64,18 @@ function validateSpecies(filename) {
   return new Promise((resolve, reject) => {
     validadeFile(filename) // chama o validateFile
       .then((output) => {
-        tplScraper(output).then((scraperOutput) => { // chama tplScraper - MOCK
-          resolve(scraperOutput)
-        })
-       
+        tplScraper(output).then((scraperOutput) => {
+          // chama tplScraper - MOCK
+          // recebe do Scraper os dados das espécies e tenta salvar no banco
+          save(scraperOutput).then(() => {
+            resolve(scraperOutput);
+          });
+        });
       })
       .catch((error) => {
-        reject(error)
-      })
-  })
-
+        reject(error);
+      });
+  });
 }
 
 module.exports = { validateSpecies };
