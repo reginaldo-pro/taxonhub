d96da22a7e996d776306a3231a4c852b695b591a
tuchinski <tuchinski@alunos.utfpr.edu.br>
2021-08-29 16:50:27 -0300

    criando mock do tplScraper, e chamando ele no Tplvalidator

--- src/tplValidator.js
+++ src/tplValidator.js
@@ -1,12 +1,41 @@
 import { validadeFile } from './fileUploader';
 
+function tplScraper(speciesList){
+  // mock do tplScraper
+
+  let mockReturn = [
+    {
+      searchName: 'Eichhornia azurea',
+      returnName: 'Eichhornia azurea (Sw.) Kunth',
+      status: 'Aceito',
+      synonym: '',
+      database: 'TPL',
+      family: 'Pontederiaceae'
+    },
+    {
+      searchName: "Eichhornia azurea",
+      returnName: "Eichhornia azurea rhizantha (Seub.)",
+      status: "sinonimo",
+      synonym: "Eichhornia azurea (Sw. Kunth)",
+      database: "FDB",
+      family: "Pontederiaceae"
+    },
+  ]
+  return new Promise((resolve, reject) => {
+    resolve(mockReturn);
+  })
+}
+
 function validateSpecies(filename) {
   // Iniciando validate species
 
   return new Promise((resolve, reject) => {
-    validadeFile(filename)
+    validadeFile(filename) // chama o validateFile
       .then((output) => {
-        resolve(output)
+        tplScraper(output).then((scraperOutput) => { // chama tplScraper - MOCK
+          resolve(scraperOutput)
+        })
+       
       })
       .catch((error) => {
         reject(error)
