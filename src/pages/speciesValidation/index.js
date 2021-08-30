719b8361d7b412d9faeb2f7356247bd798026654
Enzo Italiano <enzo.ditliano@gmail.com>
2021-08-30 02:26:27 -0300

    Adiciona bloqueador do uploader quando já existe um arquivo

--- src/pages/speciesValidation/index.js
+++ src/pages/speciesValidation/index.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import '../../sass/app.scss';
 import {DropzoneArea} from 'material-ui-dropzone'
+import Button from '@material-ui/core/Button';
 
 import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class SpecieValidation extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      files: [],
+      uploaded: false
+    };
+  }
+  handleChange(files){
+    this.setState({
+      files: files,
+      uploaded: files.length ? true : false
+    });
+  }
+  removeCSV(){
+    this.setState({
+      files: [],
+      uploaded: false
+    })
+  }
   render() {
     return (
       <div className="speciesScreen">
@@ -19,8 +39,19 @@ class SpecieValidation extends Component {
         </nav>
 
         <div className="speciesScreenTitle">
-          <h1>Inclua o CSV pelo botão abaixo para validar as especies</h1>
-          <DropzoneArea />
+        {this.state.uploaded ? (
+          <>
+            <h1>Remova o CSV atual para utilizar outro</h1>
+            <Button variant="contained" color="secondary" onClick={this.removeCSV.bind(this)}>
+              Remover
+            </Button>
+          </>
+        ) : (
+          <>
+            <h1>Inclua o CSV abaixo para validar as espécies</h1>
+            <DropzoneArea onChange={this.handleChange.bind(this)}/>
+          </>
+        )}
         </div>
       </div>
     );
