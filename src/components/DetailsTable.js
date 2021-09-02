8fabc375f132d7217b3b1ca3800ce6967ff5915a
sorattorafa <soratto@alunos.utfpr.edu.br>
2021-09-02 18:40:47 -0300

    start details table

---
+++ src/components/DetailsTable.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+    padding: 2,
+  },
+});
+
+function createData(id, nome, progresso) {
+  return {
+    id, nome, progresso,
+  };
+}
+
+const rows = [
+  createData(1, 'The plant list', '100%'),
+  createData(2, 'Flora do Brasil', '100%'),
+];
+
+export default function DenseTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID </TableCell>
+            <TableCell align="right">Nome do site</TableCell>
+            <TableCell align="right">Progresso do Download </TableCell>
+            <TableCell>
+              Sinonimos
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => {
+            return (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {row.nome}
+                </TableCell>
+                <TableCell align="right">{row.progresso}</TableCell>
+                <TableCell>
+                  Texto
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,13 +1,12 @@
-
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
+import DetailsTable from './DetailsTable';
 
 export default function ResponsiveDialog() {
   const [open, setOpen] = React.useState(false);
@@ -24,21 +23,19 @@ export default function ResponsiveDialog() {
 
   return (
     <div>
-    
+
       <Button variant="contained" color="primary" onClick={handleClickOpen}>
-                                Ver Detalhes
-                              </Button>
+        Ver Detalhes
+      </Button>
       <Dialog
         fullScreen={fullScreen}
         open={open}
         onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
-        <DialogTitle id="responsive-dialog-title">{"Detalhes"}</DialogTitle>
+        <DialogTitle id="responsive-dialog-title">Detalhes</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Lista de Sinonimos
-          </DialogContentText>
+          <DetailsTable />
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose} color="primary">
